8db567b90013ddca082ac0cb2cd3725ecab42834
macssimo <macssimo@gmail.com>
2025-02-19 07:36:38 -0300

    crear mainecoonbsas
    instalacion de react

---
+++ mainecoonBsAs/src/main.jsx
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client'
+import './styles.css'
+import './reset.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render( <App /> )
